40cc64227a4c130b92a3d40d52700f8db58bf26c
Clément Robert <cr52@protonmail.com>
2020-06-26 11:41:45 +0200

    rewrite tests for nose (keep pytest-versions as comments)

--- yt/data_objects/tests/test_time_series.py
+++ yt/data_objects/tests/test_time_series.py
@@ -1,7 +1,27 @@
 from yt.data_objects.time_series import get_filenames_from_glob_pattern
-import pytest
-from pathlib import Path
 
+from pathlib import Path
+import os
+import tempfile
+from yt.testing import assert_raises
+
+def test_pattern_expansion():
+    file_list = ["fake_data_file_{}".format(str(i).zfill(4)) for i in range(10)]
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        for file in file_list:
+            (Path(tmpdir) / file).touch()
+
+        pattern = os.path.join(tmpdir, "fake_data_file_*")
+        found = get_filenames_from_glob_pattern(pattern)
+        assert found == [os.path.join(tmpdir, file) for file in file_list]
+
+        found2 = get_filenames_from_glob_pattern(Path(pattern))
+        assert found2 == found
+
+# future: equivalent implementation using pytest fixtures
+"""
+import pytest
 def test_pattern_expansion(tmpdir):
     file_list = ["fake_data_file_{}".format(str(i).zfill(4)) for i in range(10)]
     for file in file_list:
@@ -12,8 +32,17 @@ def test_pattern_expansion(tmpdir):
 
     found2 = get_filenames_from_glob_pattern(Path(pattern))
     assert found2 == found
+"""
 
+def test_no_match_pattern():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        pattern = os.path.join(tmpdir, "fake_data_file_*")
+        assert_raises(FileNotFoundError, get_filenames_from_glob_pattern, pattern)
+
+# future: equivalent implementation using pytest fixtures
+"""
 def test_no_match_pattern(tmpdir):
     pattern = tmpdir.join("fake_data_file_*")
     with pytest.raises(FileNotFoundError):
         get_filenames_from_glob_pattern(pattern)
+"""
