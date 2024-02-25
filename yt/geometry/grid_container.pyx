f0dad4fe61c2cd4ad4d8d4000f8e297d02041784
Clément Robert <cr52@protonmail.com>
2024-02-25 11:51:09 +0100

    BLD: np.int_t -> np.int64_t

--- yt/geometry/grid_container.pyx
+++ yt/geometry/grid_container.pyx
@@ -60,7 +60,7 @@ cdef class GridTree:
                   np.ndarray[np.int64_t, ndim=1] num_children):
 
         cdef int i, j, k
-        cdef np.ndarray[np.int_t, ndim=1] child_ptr
+        cdef np.ndarray[np.int64_t, ndim=1] child_ptr
 
         child_ptr = np.zeros(num_grids, dtype='int')
 
