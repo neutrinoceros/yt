b53d3d6682d8697edab9c737cbf8b2f89319a1e8
Clément Robert <cr52@protonmail.com>
2024-03-18 20:34:40 +0100

    exp: try uv in CI

--- tests/ci_install.sh
+++ tests/ci_install.sh
@@ -5,11 +5,6 @@ cp tests/matplotlibrc .
 
 # Step 1: pre-install required packages
 if [[ ${dependencies} == "full" || ${dependencies} == "cartopy" ]]; then
-    # upgrading pip to guarantee installing extra dependencies with [full] is supported
-    # this is only necessary for some versions of Python 3.8 and 3.9
-    # see https://github.com/yt-project/yt/issues/4270
-    python -m pip install 'pip>=21.2'
-
     case ${RUNNER_OS} in
     linux|Linux)
         sudo apt-get -qqy update
@@ -32,18 +27,18 @@ fi
 # but the primary intention is to embed this script in CI jobs
 if [[ ${dependencies} == "minimal" ]]; then
     # test with minimal versions of runtime dependencies
-    python -m pip install -e ".[test,minimal]"
+    uv pip install -e ".[test,minimal]"
 elif [[ ${dependencies} == "cartopy" ]]; then
-    python -m pip install 'cartopy>=0.22'
+    uv pip install 'cartopy>=0.22'
     # scipy is an optional dependency to cartopy
-    python -m pip install scipy
-    python -m pip install -e ".[test]"
+    uv pip install scipy
+    uv pip install -e ".[test]"
 elif [[ ${dependencies} == "full" ]]; then
     # test with all optional runtime dependencies
-    python -m pip install -e ".[test,full]"
+    uv pip install -e ".[test,full]"
 else
    # test with no special requirements
-   python -m pip install -e ".[test]"
+   uv pip install -e ".[test]"
 fi
 
 # Disable excessive output
